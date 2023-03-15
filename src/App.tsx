0dc075c3174439641493553c4358cec75e29bbda
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-03-14 21:52:34 -0500

    more styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 interface Estudiante {
   nombre: string;
   semestre: number;
@@ -12,9 +12,24 @@ interface Equipo {
 }
 
 const estudiantesIniciales: Estudiante[] = [
-  { nombre: "Juan", semestre: 4, deporteFavorito: "Fútbol", equipos: ["Real Madrid", "Barcelona"] },
-  { nombre: "María", semestre: 6, deporteFavorito: "Tenis", equipos: ["Rafa Nadal Academy", "Roger Federer Academy"] },
-  { nombre: "Pedro", semestre: 2, deporteFavorito: "Baloncesto", equipos: ["Chicago Bulls", "Los Angeles Lakers"] }
+  {
+    nombre: "Juan",
+    semestre: 4,
+    deporteFavorito: "Fútbol",
+    equipos: ["Real Madrid", "Barcelona"],
+  },
+  {
+    nombre: "María",
+    semestre: 6,
+    deporteFavorito: "Tenis",
+    equipos: ["Rafa Nadal Academy", "Roger Federer Academy"],
+  },
+  {
+    nombre: "Pedro",
+    semestre: 2,
+    deporteFavorito: "Baloncesto",
+    equipos: ["Chicago Bulls", "Los Angeles Lakers"],
+  },
 ];
 
 const equiposIniciales: Equipo[] = [
@@ -23,18 +38,31 @@ const equiposIniciales: Equipo[] = [
   { nombre: "Rafa Nadal Academy" },
   { nombre: "Roger Federer Academy" },
   { nombre: "Chicago Bulls" },
-  { nombre: "Los Angeles Lakers" }
+  { nombre: "Los Angeles Lakers" },
 ];
 
 const App: React.FC = () => {
-  const [estudiantes, setEstudiantes] = useState<Estudiante[]>(estudiantesIniciales);
-  const [nuevoEstudiante, setNuevoEstudiante] = useState<Estudiante>({ nombre: "", semestre: 0, deporteFavorito: "", equipos: [] });
-  const [estudianteEditado, setEstudianteEditado] = useState<Estudiante | null>(null);
+  const [estudiantes, setEstudiantes] =
+    useState<Estudiante[]>(estudiantesIniciales);
+  const [nuevoEstudiante, setNuevoEstudiante] = useState<Estudiante>({
+    nombre: "",
+    semestre: 0,
+    deporteFavorito: "",
+    equipos: [],
+  });
+  const [estudianteEditado, setEstudianteEditado] = useState<Estudiante | null>(
+    null
+  );
   const [equipos, setEquipos] = useState<Equipo[]>(equiposIniciales);
 
   const handleAgregar = () => {
     setEstudiantes([...estudiantes, nuevoEstudiante]);
-    setNuevoEstudiante({ nombre: "", semestre: 0, deporteFavorito: "", equipos: [] });
+    setNuevoEstudiante({
+      nombre: "",
+      semestre: 0,
+      deporteFavorito: "",
+      equipos: [],
+    });
   };
 
   const handleEliminar = (index: number) => {
@@ -50,15 +78,29 @@ const App: React.FC = () => {
 
   const handleGuardar = () => {
     const nuevosEstudiantes = [...estudiantes];
-    nuevosEstudiantes.splice(estudiantes.indexOf(estudianteEditado!), 1, nuevoEstudiante);
+    nuevosEstudiantes.splice(
+      estudiantes.indexOf(estudianteEditado!),
+      1,
+      nuevoEstudiante
+    );
     setEstudiantes(nuevosEstudiantes);
     setEstudianteEditado(null);
-    setNuevoEstudiante({ nombre: "", semestre: 0, deporteFavorito: "", equipos: [] });
+    setNuevoEstudiante({
+      nombre: "",
+      semestre: 0,
+      deporteFavorito: "",
+      equipos: [],
+    });
   };
 
   const handleCancelar = () => {
     setEstudianteEditado(null);
-    setNuevoEstudiante({ nombre: "", semestre: 0, deporteFavorito: "", equipos: [] });
+    setNuevoEstudiante({
+      nombre: "",
+      semestre: 0,
+      deporteFavorito: "",
+      equipos: [],
+    });
   };
 
   return (
@@ -78,125 +120,159 @@ const App: React.FC = () => {
             <tr key={index}>
               <td>{estudiante.nombre}</td>
               <td>{estudiante.semestre}</td>
-              <td>{estudiante          .deporteFavorito}</td>
-          <td>{estudiante.equipos.join(", ")}</td>
-          <td>
-            <button onClick={() => handleEditar(estudiante, index)}>Editar</button>
-            <button onClick={() => handleEliminar(index)}>Eliminar</button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-
-  {estudianteEditado && (
-    <div>
-      <h3>Editar Estudiante</h3>
-      <div>
-        <label>Nombre:</label>
-        <input
-          type="text"
-          value={nuevoEstudiante.nombre}
-          onChange={(event) => setNuevoEstudiante({ ...nuevoEstudiante, nombre: event.target.value })}
-        />
-      </div>
-      <div>
-        <label>Semestre:</label>
-        <input
-          type="number"
-          value={nuevoEstudiante.semestre}
-          onChange={(event) => setNuevoEstudiante({ ...nuevoEstudiante, semestre: Number(event.target.value) })}
-        />
-      </div>
-      <div>
-        <label>Deporte Favorito:</label>
-        <input
-          type="text"
-          value={nuevoEstudiante.deporteFavorito}
-          onChange={(event) =>
-            setNuevoEstudiante({ ...nuevoEstudiante, deporteFavorito: event.target.value })
-          }
-        />
-      </div>
-      <div>
-        <label>Equipos:</label>
-        <select
-          multiple
-          value={nuevoEstudiante.equipos}
-          onChange={(event) =>
-            setNuevoEstudiante({
-              ...nuevoEstudiante,
-              equipos: Array.from(event.target.selectedOptions, (option) => option.value)
-            })
-          }
-        >
-          {equipos.map((equipo, index) => (
-            <option key={index} value={equipo.nombre}>
-              {equipo.nombre}
-            </option>
+              <td>{estudiante.deporteFavorito}</td>
+              <td>{estudiante.equipos.join(", ")}</td>
+              <td>
+                <button onClick={() => handleEditar(estudiante, index)}>
+                  Editar
+                </button>
+                <button onClick={() => handleEliminar(index)}>Eliminar</button>
+              </td>
+            </tr>
           ))}
-        </select>
-      </div>
-      <div>
-        <button onClick={handleGuardar}>Guardar</button>
-        <button onClick={handleCancelar}>Cancelar</button>
-      </div>
-    </div>
-  )}
+        </tbody>
+      </table>
 
-  <div>
-    <h3>Agregar Estudiante</h3>
-    <div>
-      <label>Nombre:</label>
-      <input
-        type="text"
-        value={nuevoEstudiante.nombre}
-        onChange={(event) => setNuevoEstudiante({ ...nuevoEstudiante, nombre: event.target.value })}
-      />
+      {estudianteEditado && (
+        <div className="form-container">
+          <h3>Editar Estudiante</h3>
+          <div>
+            <label>Nombre:</label>
+            <input
+              type="text"
+              value={nuevoEstudiante.nombre}
+              onChange={(event) =>
+                setNuevoEstudiante({
+                  ...nuevoEstudiante,
+                  nombre: event.target.value,
+                })
+              }
+            />
+          </div>
+          <div>
+            <label>Semestre:</label>
+            <input
+              type="number"
+              value={nuevoEstudiante.semestre}
+              onChange={(event) =>
+                setNuevoEstudiante({
+                  ...nuevoEstudiante,
+                  semestre: Number(event.target.value),
+                })
+              }
+            />
+          </div>
+          <div>
+            <label>Deporte Favorito:</label>
+            <input
+              type="text"
+              value={nuevoEstudiante.deporteFavorito}
+              onChange={(event) =>
+                setNuevoEstudiante({
+                  ...nuevoEstudiante,
+                  deporteFavorito: event.target.value,
+                })
+              }
+            />
+          </div>
+          <div>
+            <label>Equipos:</label>
+            <select
+              multiple
+              value={nuevoEstudiante.equipos}
+              onChange={(event) =>
+                setNuevoEstudiante({
+                  ...nuevoEstudiante,
+                  equipos: Array.from(
+                    event.target.selectedOptions,
+                    (option) => option.value
+                  ),
+                })
+              }
+            >
+              {equipos.map((equipo, index) => (
+                <option key={index} value={equipo.nombre}>
+                  {equipo.nombre}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <button onClick={handleGuardar}>Guardar</button>
+            <button onClick={handleCancelar}>Cancelar</button>
+          </div>
+        </div>
+      )}
+
+      <div className="form-container">
+        <h3>Agregar Estudiante</h3>
+        <div>
+          <label>Nombre:</label>
+          <input
+            type="text"
+            value={nuevoEstudiante.nombre}
+            onChange={(event) =>
+              setNuevoEstudiante({
+                ...nuevoEstudiante,
+                nombre: event.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label>Semestre:</label>
+          <input
+            type="number"
+            value={nuevoEstudiante.semestre}
+            onChange={(event) =>
+              setNuevoEstudiante({
+                ...nuevoEstudiante,
+                semestre: Number(event.target.value),
+              })
+            }
+          />
+        </div>
+        <div>
+          <label>Deporte Favorito:</label>
+          <input
+            type="text"
+            value={nuevoEstudiante.deporteFavorito}
+            onChange={(event) =>
+              setNuevoEstudiante({
+                ...nuevoEstudiante,
+                deporteFavorito: event.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label>Equipos:</label>
+          <select
+            multiple
+            value={nuevoEstudiante.equipos}
+            onChange={(event) =>
+              setNuevoEstudiante({
+                ...nuevoEstudiante,
+                equipos: Array.from(
+                  event.target.selectedOptions,
+                  (option) => option.value
+                ),
+              })
+            }
+          >
+            {equipos.map((equipo, index) => (
+              <option key={index} value={equipo.nombre}>
+                {equipo.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <button onClick={handleAgregar}>Agregar</button>
+        </div>
+      </div>
     </div>
-    <div>
-      <label>Semestre:</label>
-      <input
-        type="number"
-        value={nuevoEstudiante.semestre}
-        onChange={(event) => setNuevoEstudiante({ ...nuevoEstudiante, semestre: Number(event.target.value) })}
-      />
-    </div>
-    <div>
-      <label>Deporte Favorito:</label>
-      <input
-        type="text"
-        value={nuevoEstudiante.deporteFavorito}
-        onChange={(event) =>
-          setNuevoEstudiante({ ...nuevoEstudiante, deporteFavorito: event.target.value })
-        }
-      />
-    </div>
-    <div>
-      <label>Equipos:</label>
-      <select
-        multiple
-        value={nuevoEstudiante.equipos}
-        onChange={(event) =>
-          setNuevoEstudiante({
-            ...nuevoEstudiante,
-            equipos: Array.from(event.target.selectedOptions, (option) => option.value)
-          })
-        }
-      >
-        {equipos.map((equipo, index) => (
-          <option key={index} value={equipo.nombre}>
-           {equipo.nombre}
-</option>
-))}
-</select>
-</div>
-<div>
-<button onClick={handleAgregar}>Agregar</button>
-</div>
-</div>
-</div>
-);
-}
+  );
+};
 
 export default App;
